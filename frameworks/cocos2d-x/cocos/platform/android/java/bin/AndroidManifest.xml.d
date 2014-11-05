b59edbc18ddfa24a66687ce615feda9ffa47ec36
乔书军 <shujun.qiao@chukong-inc.com>
2014-11-05 09:49:47 +0800

    delete cocos

--- frameworks/cocos2d-x/cocos/platform/android/java/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/workspace/anysdk/lua/anysdk_lua/frameworks/cocos2d-x/cocos/platform/android/java/bin/AndroidManifest.xml : \
-/workspace/anysdk/lua/anysdk_lua/frameworks/cocos2d-x/cocos/platform/android/java/AndroidManifest.xml \
--- frameworks/cocos2d-x/cocos/platform/android/java/gen/R.java.d
+++
@@ -1,1 +0,0 @@
- : /workspace/anysdk/lua/anysdk_lua/frameworks/cocos2d-x/cocos/platform/android/java/bin/AndroidManifest.xml \
